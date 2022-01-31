22800b36f10a8ca7400ee0bef0d38112e4377c72
brxoo <brunoaraujo198@gmail.com>
2022-01-31 14:24:40 -0300

    Updating Users with PUT

--- src/main/java/com/brxoo/workshop/services/UserService.java
+++ src/main/java/com/brxoo/workshop/services/UserService.java
@@ -25,17 +25,28 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repository.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 }
