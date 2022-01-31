91184045e846d2ca918904182ecbdd16109fdb05
brxoo <brunoaraujo198@gmail.com>
2022-01-31 18:00:17 -0300

    Simple query with @Query

--- src/main/java/com/brxoo/workshop/repository/PostRepository.java
+++ src/main/java/com/brxoo/workshop/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.brxoo.workshop.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.brxoo.workshop.domain.Post;
@@ -10,5 +11,8 @@ import com.brxoo.workshop.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	List<Post> findByTitleContainingIgnoreCase(String text);
+//	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
